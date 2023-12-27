c264b3ab8eef6d4d686aed85618f8490cf7c5aa7
David Collett <dcollett.seattle@gmail.com>
2023-12-27 09:33:27 -0800

    Added CSS to hide github logo

--- hello_world.py
+++ hello_world.py
@@ -2,6 +2,13 @@
 
 import streamlit as st
 
+# Hide the menu and github logo from being displayed
+# when others are viewing the page
+hide_github_icon = """<style> .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }</style>"""
+
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
+
 # Add a header to the sidebar
 st.sidebar.header('My sidebar')
 st.sidebar.markdown('Some sidebar text will go here!')
